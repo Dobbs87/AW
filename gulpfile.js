1798fe7c7f5b73eca7f3a5c6fc3603e3965955d8
Zac <zdobbs3@gmail.com>
2018-08-10 12:28:01 -0700

    added gallery styles to css folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 
 
 gulp.task('sass', function(){
-  return gulp.src('scss/styles.scss')
+  return gulp.src('scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'))
 });
