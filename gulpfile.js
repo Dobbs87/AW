cd34f28b904f6b2202dc1447ad80214d658ffe59
Zac <zdobbs3@gmail.com>
2018-08-10 19:09:22 -0700

    gulp watch fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 
-gulp.task('sass', function(){
-  return gulp.src('scss/*.scss')
-    .pipe(sass())
+gulp.task('sass', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()).on("error", sass.logError)
     .pipe(gulp.dest('css'))
+    .pipe(reload({ stream:true }));
 });
 
-gulp.task('watch', function() {
+// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
+gulp.task('serve', gulp.series('sass', function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+
   gulp.watch('scss/**/*.scss', gulp.series('sass'));
-});
+}));
